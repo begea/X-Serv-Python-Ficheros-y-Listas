0fb08713edcd6bfe8090d902e67a342a9941ba2e
begea <boegma@hotmail.com>
2017-02-01 01:37:12 +0100

    Ejercicio con la función find()

---
+++ funct_find.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+fd = open("/etc/passwd", "r")
+
+lineas = fd.readlines()
+fd.close()
+
+for linea in lineas:
+  posicion1 = linea.find(":")
+  posicion2 = linea.rfind(":")
+  print(linea[: posicion1] + " " + linea[posicion2+1 :-1]);
